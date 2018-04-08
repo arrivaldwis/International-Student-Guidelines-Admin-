5b3e537875eca11f32afd5ae19f2e97f29a9a8b2
Arrival Dwi Sentosa <arrivaldwisentosa@gmail.com>
2018-04-08 13:45:11 +0800

    first commit

---
+++ app/src/main/java/com/sandywinata/isgupdate/AddUser.java
@@ -0,0 +1,13 @@
+package com.sandywinata.isgupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
---
+++ app/src/main/java/com/sandywinata/isgupdate/ChatList.java
@@ -0,0 +1,13 @@
+package com.sandywinata.isgupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
