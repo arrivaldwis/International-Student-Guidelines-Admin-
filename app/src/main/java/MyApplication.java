74353374e1399aa609ba1fb685d437ff216bd633
Arrival Dwi Sentosa <arrivaldwisentosa@gmail.com>
2018-05-13 23:16:57 +0800

    Update Intent

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,10 @@
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+    }
+}
--- app/src/main/java/com/sandywinata/isgupdate/MainActivity.java
+++ app/src/main/java/com/sandywinata/isgupdate/MainActivity.java
@@ -1,13 +1,69 @@
 package com.sandywinata.isgupdate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.cvAcademicCal)
+    CardView cvAcademicCal;
+    @BindView(R.id.cvAccomodation)
+    CardView cvAccomodation;
+    @BindView(R.id.cvChat)
+    CardView cvChat;
+    @BindView(R.id.cvContact)
+    CardView cvContact;
+    @BindView(R.id.cvAddUser)
+    CardView cvAddUser;
+    @BindView(R.id.cvPointofInterest)
+    CardView cvPointofInterest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.cvAcademicCal)
+    public void academic() {
+        Intent intent = new Intent(MainActivity.this, AcademicCal.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.cvAccomodation)
+    public void accommodation() {
+        Intent intent = new Intent(MainActivity.this, Accomodation.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.cvContact)
+    public void contact() {
+        Intent intent = new Intent(MainActivity.this, Contacts.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.cvAddUser)
+    public void adduser() {
+        Intent intent = new Intent(MainActivity.this, AddUser.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.cvPointofInterest)
+    public void poi() {
+        Intent intent = new Intent(MainActivity.this, PointOfInterest.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.cvChat)
+    public void chat() {
+        Intent intent = new Intent(MainActivity.this, ChatList.class);
+        startActivity(intent);
     }
 }
